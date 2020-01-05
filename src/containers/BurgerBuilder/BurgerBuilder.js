09bf3c095f404c58a5213a8593ddc31be5ac0395
wangxuefei <xuefeiwang@jinshuju.net>
2020-01-05 15:49:14 +0800

    add basic components and containers

---
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+class BurgerBuilder extends React.Component {
+  render() {
+    return (
+      <>
+        <div>Burger Prensenter</div>
+        <div>Burger Builder Controllers</div>
+      </>
+    );
+  }
+}
+
+export default BurgerBuilder;
