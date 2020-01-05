640f3b5c3095b292be7b43eaf34d3e1ded7da6d9
wangxuefei <xuefeiwang@jinshuju.net>
2020-01-05 19:56:10 +0800

    calculate sum dynamically

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -3,13 +3,19 @@ import BurgerIngredient from "./BurgerIngredient/BurgerIngredient";
 import classes from "./Burger.module.css";
 
 const burger = props => {
-  const innerIngredients = Object.entries(props.ingredients).map(
-    ([ingredient, num]) => {
+  let innerIngredients = Object.entries(props.ingredients)
+    .map(([ingredient, num]) => {
       return [...Array(num)].map((_, i) => {
         return <BurgerIngredient type={ingredient} key={ingredient + i} />;
       });
-    }
-  );
+    })
+    .reduce((arr, el) => {
+      return arr.concat(el);
+    }, []);
+
+  if (innerIngredients.length === 0) {
+    innerIngredients = <p>Please start adding ingredients!!</p>;
+  }
 
   return (
     <div className={classes.Burger}>
